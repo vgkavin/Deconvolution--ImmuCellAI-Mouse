20c154a0f67829ba5741c7f0509ea8c1ba339e3c
Kavin Velur Ganesan <vgkavin@gmail.com>
2024-11-11 18:45:27 -0500

    ImmucellAI_mouse.R

--- ImmucellAI_mouse.R
+++ ImmucellAI_mouse.R
@@ -42,50 +42,45 @@ result <- ImmuCellAI_mouse(data, "rnaseq", 1, 0)
 saveRDS(result, "result.rds")
 write.csv(result$abundance, "Abundance.csv")
 
+#optional- Load the results
+result<- readRDS("result.rds")
+
 #Downstream analysis and visualization of individual and group results
 #applicable only if run in group mode
-
-#save individual and group results as separate dataframes
 abundance <- as.data.frame(result$abundance)
 group <- as.data.frame(result$group_result)
-#mark individual data with their respective groups
-abundance$Group <- rep(c("Control", "Treatment1", "Treatment2"), c(10, 4, 4))
+abundance$Group <- rep(c("Sham", "D7", "D70"), c(10, 4, 4))
 abundance$Sample <- rownames(abundance)
 group$Group <- rownames(group)
+group_means <- abundance %>%
+  group_by(Group) %>%
+  summarise(across(B_cell:Infiltration_score, mean, na.rm = TRUE))
 
-
-#rearrange individual and group abundace to match each sample/group with their celltype abundances
 abundance_long <- gather(abundance, key = "Cell_Type", value = "Abundance", -Sample, -Group)
 group_long <- group %>%
   pivot_longer(cols = -Group, names_to = "Cell_Type", values_to = "Mean_Abundance")
 
-#create an individual object and store all p values to display in graph
+
 p_value <- group_long %>% filter(Group == "p value")
 p_value$p_value <- p_value$Mean_Abundance
 p_values <- p_value %>%
   mutate(p_value_label = paste("p =", p_value))
-
-#remove the p-value column from rearranged group results
 group_long <- group_long %>% filter(Group != "p value")
-
-#create an individual vector and store all the cell types
 cell_types <- unique(abundance_long$Cell_Type)
 
-#define levels
-group_long$Group <- factor(group_long$Group, levels = c("Control", "Treatment1", "Treatment2"))
-abundance_long$Group <- factor(abundance_long$Group, levels = c("Control", "Treatment1", "Treatment2"))
+group_long$Group <- factor(group_long$Group, levels = c("Sham", "D7", "D70"))
+abundance_long$Group <- factor(abundance_long$Group, levels = c("Sham", "D7", "D70"))
 
-
-#make boxplots for each celltype with their respective p values and combine them in a single frame
+#create box plots for all celltypes in a single graph
 All_plots <- ggplot() +
-  geom_boxplot(data = abundance_long, aes(x = Group, y = Abundance, fill = Group)) + #make boxplot
+  geom_boxplot(data = abundance_long, aes(x = Group, y = Abundance, fill = Group)) +
   geom_errorbar(data = group_long, 
                 aes(x = Group, ymin = Mean_Abundance, ymax = Mean_Abundance), 
-                width = 0.3, color = "red", linewidth = 0.8) + # Custom mean lines
+                width = 0.3, color = "red", size = 0.8) + # Custom mean lines
   facet_wrap(~ Cell_Type, scales = "free_y") +
   geom_text(data = p_values, aes(x = Inf, y = Inf, label = p_value_label), 
-            hjust = 2, vjust = 1.5, size = 2.5, color = "red") +                #display p value
-  scale_fill_manual(values = c("Control" = "yellow", "Treatment1" = "darkgreen", "Treatment2" = "orange")) + # Custom colors
+            hjust = 2, vjust = 1.5, size = 2.5, color = "red") + 
+  scale_fill_manual(values = c("Sham" = "yellow", "D7" = "darkgreen", "D70" = "orange")) + # Custom colors
   theme_minimal() +
   theme(
     axis.text.x = element_text(angle = 90, hjust = 1),
@@ -100,66 +95,62 @@ All_plots <- ggplot() +
     axis.ticks = element_line(color = "black", size = 0.5), # Add major ticks
     axis.ticks.length = unit(0.3, "cm"), # Adjust length of major ticks
   ) +
-  labs(title = "Boxplots and Means of Cell Types by group", y = "ImmuCellAI-Mouse.Score", x = "Treatment")
+  labs(title = "Boxplots and Means of Cell Types by Treatment- n(Sham)=10", y = "ImmuCellAI-Mouse.Score", x = "Treatment")
+
+#save the combined plot as PNG and SVG
+ggsave(filename = "plots/ImmuCellAI_result_sham-n(10).png", plot = All_plots, width = 12, height = 8, dpi = 300)
+ggsave(filename = "plots/ImmuCellAI_result_sham-n(10).svg", plot = All_plots, device = "svg", width = 12, height = 8, dpi = 300)
 
 
-#save the image as high quality .png file
-ggsave(filename = "ImmuCellAI_result_sham.png", plot = All_plots, width = 12, height = 8, dpi = 300)
+#create an empty list to store all indivdual plot data
+plot_list <- list()
+
+#for loop to make plots for individual cell types and store in list
+for (i in cell_types) {
+  plot_list[[i]] <-  ggplot() +
+    geom_boxplot(data = subset(abundance_long, Cell_Type == i), 
+                 aes(x = Group, y = Abundance, fill = Group)) +
+    geom_errorbar(data = subset(group_long, Cell_Type == i), 
+                  aes(x = Group, ymin = Mean_Abundance, ymax = Mean_Abundance), 
+                  width = 0.3, color = "red", size = 0.8) + # Custom mean lines
+    geom_text(data = subset(p_values, Cell_Type == i), 
+              aes(x = Inf, y = Inf, label = p_value_label), 
+              hjust = 2.25, vjust = 1.5, size = 4, color = "red") +
+    scale_fill_manual(values = c("Sham" = "yellow", "D7" = "darkgreen", "D70" = "orange")) + # Custom colors
+    theme_minimal() +
+    theme(
+      axis.text.x = element_text(angle = 90, hjust = 1),
+      plot.title = element_text(hjust = 0.5),
+      panel.grid.major = element_blank(),   # Remove major grid lines
+      panel.grid.minor = element_blank(),   # Remove minor grid lines
+      panel.border = element_blank(),       # Remove panel border
+      plot.background = element_rect(fill = "white"),  # Set plot background to white
+      panel.background = element_rect(fill = "white"), # Set panel background to white
+      axis.line.x.bottom = element_line(color = "black", size = 0.5), # Add x-axis line at the bottom
+      axis.line.y.left  = element_line(color = "black", size = 0.5), # Add y-axis line on the left
+      axis.ticks = element_line(color = "black", size = 0.5), # Add major ticks
+      axis.ticks.length = unit(0.3, "cm"), # Adjust length of major ticks
+    ) +
+    labs(title = i, y = "ImmuCellAI-Mouse.Score", x = "Treatment")
+}
 
 
-#make separate boxplots for individual celltypes
-Monocytes <- ggplot() +
-  geom_boxplot(data = subset(abundance_long, Cell_Type == "Monocytes"), 
-               aes(x = Group, y = Abundance, fill = Group)) +
-  geom_errorbar(data = subset(group_long, Cell_Type == "Monocytes"), 
-                aes(x = Group, ymin = Mean_Abundance, ymax = Mean_Abundance), 
-                width = 0.3, color = "red", size = 0.8) + # Custom mean lines
-  geom_text(data = subset(p_values, Cell_Type == "Monocytes"), 
-            aes(x = Inf, y = Inf, label = p_value_label), 
-            hjust = 2.25, vjust = 1.5, size = 4, color = "red") +
-  scale_fill_manual(values = c("Control" = "yellow", "Treatment1" = "darkgreen", "Treatment2" = "orange")) + # Custom colors
-  theme_minimal() +
-  theme(
-    axis.text.x = element_text(angle = 90, hjust = 1),
-    plot.title = element_text(hjust = 0.5),
-    panel.grid.major = element_blank(),   # Remove major grid lines
-    panel.grid.minor = element_blank(),   # Remove minor grid lines
-    panel.border = element_blank(),       # Remove panel border
-    plot.background = element_rect(fill = "white"),  # Set plot background to white
-    panel.background = element_rect(fill = "white"), # Set panel background to white
-    axis.line.x.bottom = element_line(color = "black", size = 0.5), # Add x-axis line at the bottom
-    axis.line.y.left  = element_line(color = "black", size = 0.5), # Add y-axis line on the left
-    axis.ticks = element_line(color = "black", size = 0.5), # Add major ticks
-    axis.ticks.length = unit(0.3, "cm"), # Adjust length of major ticks
-  ) +
-  labs(title = "Monocytes", y = "ImmuCellAI-Mouse.Score", x = "Treatment")
-
-Neutrophils <- ggplot() +
-  geom_boxplot(data = subset(abundance_long, Cell_Type == "Neutrophils"), 
-               aes(x = Group, y = Abundance, fill = Group)) +
-  geom_errorbar(data = subset(group_long, Cell_Type == "Neutrophils"), 
-                aes(x = Group, ymin = Mean_Abundance, ymax = Mean_Abundance), 
-                width = 0.3, color = "red", size = 0.8) + # Custom mean lines
-  geom_text(data = subset(p_values, Cell_Type == "Neutrophils"), 
-            aes(x = Inf, y = Inf, label = p_value_label), 
-            hjust = 2.25, vjust = 1.5, size = 4, color = "red") +
-  scale_fill_manual(values = c("Control" = "yellow", "Treatment1" = "darkgreen", "Treatment2" = "orange")) + # Custom colors
-  theme_minimal() +
-  theme(
-    axis.text.x = element_text(angle = 90, hjust = 1),
-    plot.title = element_text(hjust = 0.5),
-    panel.grid.major = element_blank(),   # Remove major grid lines
-    panel.grid.minor = element_blank(),   # Remove minor grid lines
-    panel.border = element_blank(),       # Remove panel border
-    plot.background = element_rect(fill = "white"),  # Set plot background to white
-    panel.background = element_rect(fill = "white"), # Set panel background to white
-    axis.line.x.bottom = element_line(color = "black", size = 0.5), # Add x-axis line at the bottom
-    axis.line.y.left  = element_line(color = "black", size = 0.5), # Add y-axis line on the left
-    axis.ticks = element_line(color = "black", size = 0.5), # Add major ticks
-    axis.ticks.length = unit(0.3, "cm"), # Adjust length of major ticks
-  ) +
-  labs(title = "Neutrophils", y = "ImmuCellAI-Mouse.Score", x = "Treatment")
+# Loop through each plot in plot_list and save as SVG
+for (i in names(plot_list)) {
+  # Define the filename using the cell type name
+  file_name <- paste0("plots/plot_", i, ".svg")
+  
+  # Save each plot as a SVG file
+  ggsave(filename = file_name, plot = plot_list[[i]], device = "svg", width = 8, height = 6, dpi = 300)
+}
 
 
-#organize indiviual celltype boxplots in grids
-plot_grid(Monocytes, Neutrophils, nrow = 1, ncol = 2)
+# Loop through each plot in plot_list and save as PNG
+for (i in names(plot_list)) {
+  # Define the filename using the cell type name
+  file_name <- paste0("plots/plot_", i, ".png")
+  
+  # Save each plot as a PNG file
+  ggsave(filename = file_name, plot = plot_list[[i]], device = "png", width = 8, height = 6, dpi = 300)
+}
+
